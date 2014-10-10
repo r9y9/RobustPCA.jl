3b800c2617b04ab936afcde623009a86f5e0e4c8
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-11 00:22:35 +0900

    Follows the reference paper notations and fix some comments

--- src/inexact_alm.jl
+++ src/inexact_alm.jl
@@ -1,5 +1,7 @@
-# Robust Principal Component Analysis (RPCA) using the inexact augumented
-# Lagrange multiplier.
+# Soft thresholding function
+soft_threshold(x, ϵ::Float64) = max(x - ϵ , 0) + min(x - ϵ, 0)
+
+# RPCA using the inexact Augumented Lagrange Multiplier (ALM).
 # Given a observation matrix D, find row-rank matrix A and sparse matrix E
 # so that D = A + E.
 function rpca_inexact_alm(D::AbstractMatrix;
@@ -22,17 +24,17 @@ function rpca_inexact_alm(D::AbstractMatrix;
     const μ̄ = μ⁰ * 1.0e+7
 
     converged = false
-    epoch = 0
+    k = 0
     sv⁰ = 10
 
     Yᵏ, Aᵏ, Eᵏ, μᵏ, svᵏ = Y⁰, A⁰, E⁰, μ⁰, sv⁰
     while !converged
         # update sparse matrix E
-        temp_T = D - Aᵏ + μᵏ^-1 * Yᵏ
-        Eᵏ = max(temp_T - λ / μᵏ, 0) + min(temp_T + λ / μᵏ, 0)
-
+        Eᵏ = soft_threshold(D - Aᵏ + μᵏ^-1 * Yᵏ, λ * μᵏ^-1)
+        
         # force non-negative
         Eᵏ = max(Eᵏ, 0) # heuristic
+
         U, S, V = svd(D - Eᵏ + μᵏ^-1 * Yᵏ)
 
         # trancate dimention
@@ -43,19 +45,19 @@ function rpca_inexact_alm(D::AbstractMatrix;
             svᵏ = min(svpᵏ + round(0.05 * N), N)
         end
 
-        # update A
+        # update row-rank matrix A
         Aᵏ = U[:,1:svpᵏ] * diagm(S[1:svpᵏ] - μᵏ^-1) * V[:,1:svpᵏ]'
-
         # force non-negative
         Aᵏ = max(Aᵏ, 0)
 
         Z = D - Aᵏ - Eᵏ;
+
         Yᵏ = Yᵏ + μᵏ * Z;
         μᵏ = min(μᵏ * ρ, μ̄)
 
         objective = norm(Z) / d_norm;
         if verbose
-            println("#$(epoch) objective: $(objective)")
+            println("#$(k) objective: $(objective)")
         end
 
         if objective < error_tol
@@ -65,8 +67,8 @@ function rpca_inexact_alm(D::AbstractMatrix;
             converged = true
         end
         
-        epoch += 1
-        if epoch >= max_iter
+        k = k + 1
+        if k >= max_iter
             break
         end
     end
