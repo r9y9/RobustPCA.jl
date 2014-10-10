632a5083dd1cc16d2ce489bc9b685dc9602c40ff
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-11 00:23:46 +0900

    Add reference paper in RPCA.jl

--- src/RPCA.jl
+++ src/RPCA.jl
@@ -1,5 +1,13 @@
 module RPCA
 
+# Robust Principal Component Analysis (RPCA) in Julia
+
+# Reference:
+# Lin, Zhouchen, Minming Chen, and Yi Ma. "The augmented lagrange multiplier
+# method for exact recovery of corrupted low-rank matrices." arXiv preprint
+# arXiv:1009.5055 (2010)
+# http://arxiv.org/pdf/1009.5055.pdf)
+
 export rpca_inexact_alm
 
 include("inexact_alm.jl")
